b7fe202acef002ce3a07c6b00588311de32cfc00
Hassan Ali <hassanmughal085870@gmail.com>
2026-09-05 12:22:22 +0500

    Components

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+import Footer from "./Components/Footer.jsx";
+import Navbar from "./Components/Navbar.jsx"
+function App(){
+  return(
+    <div>
+      <Navbar/>
+      <h1>Hello today We Will Do Components</h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "Navbar.css"
+
+function Navbar() {
+  return (
+    <div>
+      <ol>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Navbar;
